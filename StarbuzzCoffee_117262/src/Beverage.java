364cb7da977abbd7d98aa28aac6c4de6b1c42233
PaulinaRayos <163494315+PaulinaRayos@users.noreply.github.com>
2025-02-10 23:50:32 -0700

    PatronDecorador

---
+++ StarbuzzCoffee_117262/src/Beverage.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pauli
+ */
+public class Beverage implements IBeverage{
+    public String getDescription() {
+        return description;
+    }
+    
+}
---
+++ StarbuzzCoffee_117262/src/IBeverage.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author pauli
+ */
+public interface IBeverage {
+    String getDescription();
+    double getCost();
+}
